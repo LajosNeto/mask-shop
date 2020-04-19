44a670c861693812abe3c7ba0e5da93e806fa93d
Lajos Neto <lajosnetogit@gmail.com>
2020-04-19 18:38:23 -0300

    Add initial detectors implementations

---
+++ maskshop/detector.py
@@ -0,0 +1,55 @@
+"""
+Image detectors.
+Includes a detector for face box detection and facial expression detector.
+"""
+
+# Author:
+# Lajos Neto <lajosneto@gmail.com>
+
+
+import numpy as np
+import cv2
+
+from tensorflow import keras
+
+
+LABELS = {0:'angry', 1:'disgust', 2:'fear', 3:'happy', 4:'sad', 5:'surprise', 6:'neutral'}
+
+
+class DNNFaceDetector():
+    def __init__(self):
+        model_file = 'utils/opencv_face_detector_uint8.pb'
+        config_file = 'utils/opencv_face_detector.pbtxt'
+        self.net = cv2.dnn.readNetFromTensorflow(model_file, config_file)
+    
+    def run(self, image):
+        blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), [104, 117, 123], False, False)
+        self.net.setInput(blob)
+        # detect face position within the image frame
+        detections = self.net.forward()
+        (h, w) = image.shape[:2]
+        box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+        # extract the detected face (only face portion)
+        face_image = image[startY:endY, startX:endX]
+        # draw rectangle around detected face (original image)
+        box_image = cv2.rectangle(image,(startX,startY),(endX,endY),(255,0,0),2)
+        return (box_image, face_image)
+
+class ExpressionDetector():
+    def __init__(self):
+        model_file = 'model_output/face_detection.h5'
+        self.net = keras.models.load_model(model_file)
+    
+    def pre_process(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, dsize=(48,48), interpolation=cv2.INTER_CUBIC)
+        image = np.expand_dims(image,[0,3])
+        image = (image - np.mean(image))/np.std(image)
+        return image
+    
+    def run(self, image):
+        image = self.pre_process(image)
+        predictions = self.net.predict(image)
+        predicted_label =  LABELS.get(np.argmax(predictions))
+        return (predicted_label, predictions)
